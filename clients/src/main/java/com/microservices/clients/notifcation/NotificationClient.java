b4ed06045a054d1280d81220024bff0eb90c5a98
Kamal Kirosingh <kamal.kirosingh16@gmail.com>
2023-07-26 17:47:39 +0100

    removed apigw and eureka-server configs

--- clients/src/main/java/com/microservices/clients/notifcation/NotificationClient.java
+++ clients/src/main/java/com/microservices/clients/notifcation/NotificationClient.java
@@ -3,7 +3,10 @@ package com.microservices.clients.notifcation;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(value = "notification")
+@FeignClient(
+        name = "notification",
+        url = "${clients.notification.url}"
+)
 public interface NotificationClient {
 
     @GetMapping(path = "api/v1/notification")
--- notification/src/main/java/com/microservices/notification/NotificationApplication.java
+++ notification/src/main/java/com/microservices/notification/NotificationApplication.java
@@ -2,6 +2,8 @@ package com.microservices.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(
         scanBasePackages = {
@@ -9,7 +11,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
                 "com.microservices.notification"
         }
 )
-
+@PropertySources({
+        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
+})
 public class NotificationApplication {
     public static void main(String[] args) {
         System.out.println("Hello world!");
